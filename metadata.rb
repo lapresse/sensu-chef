d0fd6aaae9b2a58496108de4fa94ea282f02b106
Jean-Francois Theroux <jtheroux@lapresse.ca>
2013-02-12 10:44:32 -0500

    merged SLES hacks into official cookbook version 0.4.0. also bumped version to 0.4.1

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,16 @@ maintainer_email "chefs@sonian.net"
 license          "Apache 2.0"
 description      "Installs/Configures Sensu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.1"
+version          "0.4.1"
 
 # available @ http://community.opscode.com/cookbooks/apt
 depends "apt"
 
 # available @ http://community.opscode.com/cookbooks/yum
 depends "yum"
+
+# available @ http://community.opscode.com/cookbooks/windows
+depends "windows"
 
 # available @ http://community.opscode.com/cookbooks/rabbitmq
 depends "rabbitmq"
@@ -19,6 +22,7 @@ depends "rabbitmq"
 depends "redis"
 
 %w[
+  suse
   ubuntu
   debian
   centos
