0d0ae369e3c478baec59de5ae36fc91342df5651
Benoit Caron <bcaron@lapresse.ca>
2013-02-19 13:46:37 -0500

    Install RPM RFN on Suse

--- recipes/_linux.rb
+++ recipes/_linux.rb
@@ -25,7 +25,7 @@ when 'suse'
   # vu que le cookbook officiel ne supporte pas SuSE..
   if node[:platform_family] == 'suse'
     cookbook_file '/var/tmp/sensu-0.9.9-1.x86_64.rpm' do
-      notifies :run, 'execute[install_rpm]'
+      notifies :run, 'execute[install_rpm]', :immediately
     end
 
     execute 'install_rpm' do
